1be71ce7bcea5e619272e2335de5d58e84980c45
Beerendan <brendanrbowen@gmail.com>
2023-06-19 16:37:24 -0400

    fixed data loading, still need to fix date display and textBG

--- script.js
+++ script.js
@@ -14,65 +14,65 @@ console.log("start rowStatus");
 rowStatus();
 console.log("rowStatus done");
 
-//buttons
+//schedule item saving
 $("#9am-save").click(function(){
-  let text=$("#9am-text").val();
+  let text=$("#9am-textArea").val();
   planner[0]=text;
   save();
   console.log("Contents saved.");
 })
 
 $("#10am-save").click(function(){
-  let text=$("#10am-text").val();
+  let text=$("#10am-textArea").val();
   planner[1]=text;
   save();
   console.log("Contents saved.");
 })
 
 $("#11am-save").click(function(){
-  let text=$("#11am-text").val();
+  let text=$("#11am-textArea").val();
   planner[2]=text;
   save();
   console.log("Contents saved.");
 })
 
 $("#12pm-save").click(function(){
-  let text=$("#12pm-text").val();
+  let text=$("#12pm-textArea").val();
   planner[3]=text;
   save();
   console.log("Contents saved.");
 })
 
 $("#1pm-save").click(function(){
-  let text=$("#1pm-text").val();
+  let text=$("#1pm-textArea").val();
   planner[4]=text;
   save();
   console.log("Contents saved.");
 })
 
 $("#2pm-save").click(function(){
-  let text=$("#2pm-text").val();
+  let text=$("#2pm-textArea").val();
   planner[5]=text;
   save();
   console.log("Contents saved.");
 })
 
 $("#3pm-save").click(function(){
-  let text=$("#3pm-text").val();
+  let text=$("#3pm-textArea").val();
   planner[6]=text;
   save();
   console.log("Contents saved.");
 })
 
 $("#4pm-save").click(function(){
-  let text=$("#4pm-text").val();
+  let text=$("#4pm-textArea").val();
   planner[7]=text;
   save();
   console.log("Contents saved.");
 })
 
 $("#5pm-save").click(function(){
-  let text=$("#5pm-text").val();
+  let text=$("#5pm-textArea").val();
   planner[8]=text;
   save();
   console.log("Contents saved.");
@@ -82,25 +82,26 @@ $("#5pm-save").click(function(){
 
 //load local storage for daily planner
 function loader(){
-  planner=JSON.parse(local.getItem("planner") ||"[]");
-  $("#9am-text").val(planner[0]);
-  $("#10am-text").val(planner[1]);
-  $("#11am-text").val(planner[2]);
-  $("#12pm-text").val(planner[3]);
-  $("#1pm-text").val(planner[4]);
-  $("#2pm-text").val(planner[5]);
-  $("#3pm-text").val(planner[6]);
-  $("#4pm-text").val(planner[7]);
-  $("#5pm-text").val(planner[8]);
+  planner=JSON.parse(local.getItem("planner") || "[]");
+  $("#9am-textArea").val(planner[0]);
+  $("#10am-textArea").val(planner[1]);
+  $("#11am-textArea").val(planner[2]);
+  $("#12pm-textArea").val(planner[3]);
+  $("#1pm-textArea").val(planner[4]);
+  $("#2pm-textArea").val(planner[5]);
+  $("#3pm-textArea").val(planner[6]);
+  $("#4pm-textArea").val(planner[7]);
+  $("#5pm-textArea").val(planner[8]);
+  console.log("Schedule loaded")
 }
 
 //save all
 function save(){
-  local.setItem("planner",Json.stringify(planner));
+  local.setItem("planner",JSON.stringify(planner));
 }
 
 //past, present and future timeslot background colours
-function colours(){
+function rowStatus(){
   let time1="9:00am";
   let check1=compareTime(time1);
 
@@ -134,73 +135,73 @@ function colours(){
   //functionality for determining bg colours
 
   if  (check1== false && check2==false){
-    $("#9am-text").addClass("bg-danger");
+    $("#9am-text").addClass("bg-secondary");
   } else if (check1==false && check2==true){
-    $("#9am-text").addClass("bg-warning");
+    $("#9am-text").addClass("bg-danger");
   } else {
     $("#9am-text").addClass("bg-success");
   }
 
   if  (check2== false && check3==false){
-    $("#10am-text").addClass("bg-danger");
+    $("#10am-text").addClass("bg-secondary");
   } else if (check2==false && check3==true){
-    $("#10am-text").addClass("bg-warning");
+    $("#10am-text").addClass("bg-danger");
   } else {
     $("#10am-text").addClass("bg-success");
   }
 
   if  (check3== false && check4==false){
-    $("#11am-text").addClass("bg-danger");
+    $("#11am-text").addClass("bg-secondary");
   } else if (check3==false && check4==true){
-    $("#11am-text").addClass("bg-warning");
+    $("#11am-text").addClass("bg-danger");
   } else {
     $("#11am-text").addClass("bg-success");
   }
 
   if  (check4== false && check5==false){
-    $("#12pm-text").addClass("bg-danger");
+    $("#12pm-text").addClass("bg-secondary");
   } else if (check4==false && check5==true){
-    $("#12pm-text").addClass("bg-warning");
+    $("#12pm-text").addClass("bg-danger");
   } else {
     $("#12pm-text").addClass("bg-success");
   }
 
   if  (check5== false && check6==false){
-    $("#1pm-text").addClass("bg-danger");
+    $("#1pm-text").addClass("bg-secondary");
   } else if (check5==false && check6==true){
-    $("#1pm-text").addClass("bg-warning");
+    $("#1pm-text").addClass("bg-danger");
   } else {
     $("#1pm-text").addClass("bg-success");
   }
 
   if  (check6== false && check7==false){
-    $("#2pm-text").addClass("bg-danger");
+    $("#2pm-text").addClass("bg-secondary");
   } else if (check6==false && check7==true){
-    $("#2pm-text").addClass("bg-warning");
+    $("#2pm-text").addClass("bg-danger");
   } else {
     $("#2pm-text").addClass("bg-success");
   }
 
   if  (check7== false && check8==false){
-    $("#3pm-text").addClass("bg-danger");
+    $("#3pm-text").addClass("bg-secondary");
   } else if (check7==false && check8==true){
-    $("#3pm-text").addClass("bg-warning");
+    $("#3pm-text").addClass("bg-danger");
   } else {
     $("#3pm-text").addClass("bg-success");
   }
 
   if  (check8== false && check9==false){
-    $("#4pm-text").addClass("bg-danger");
+    $("#4pm-text").addClass("bg-secondary");
   } else if (check8==false && check9==true){
-    $("#4pm-text").addClass("bg-warning");
+    $("#4pm-text").addClass("bg-danger");
   } else {
     $("#4pm-text").addClass("bg-success");
   }
 
   if  (check9== false && check10==false){
-    $("#5pm-text").addClass("bg-danger");
+    $("#5pm-text").addClass("bg-secondary");
   } else if (check9==false && check10==true){
-    $("#5pm-text").addClass("bg-warning");
+    $("#5pm-text").addClass("bg-danger");
   } else {
     $("#5pm-text").addClass("bg-success");
   }
